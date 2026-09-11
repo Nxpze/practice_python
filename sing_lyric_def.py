7eba165cdaec4598646cc742592ea5ece00a3ae6
Nxpze <nnnopphara.t2549@gmail.com>
2026-09-11 14:39:08 +0700

    add for loop

--- sing_lyric_def.py
+++ sing_lyric_def.py
@@ -5,3 +5,8 @@ from animate_text import animate_text
 def sing_lyric(lyric, delay, speed):
     time.sleep(delay)
     animate_text(lyric, speed)
+
+
+
+for i in range(1,41):
+    print("one sample wilcoxon signed rank test ใช้สำหรับทดสอบสมมติฐาน 1 ประชากร")
